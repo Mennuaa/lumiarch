bde827404b473691efc003530b4d2b07f935fd6d
mangasar7an <mangasaryan1648@gmail.com>
2024-05-16 16:49:55 +0400

    fons

---
+++ src/components/Object/Object.jsx
@@ -0,0 +1,142 @@
+import React, { useState } from 'react'
+import './object.css'
+
+export const Object = ({ onChangeSlide }) => {
+	const slides = [
+		{
+			first_text: "Инвестиции в недвижимость Камбоджи 'под ключ'",
+			second_text:
+				'Мы предлагаем: гарантированный возврат инвестиций в недвижимость Пном Пеня напрямую от застройщика',
+			background: '/mobile/wealth mansion/wm_back.webp',
+			icons: ['/images/icon1.svg', '/images/icon2.svg', '/images/icon3.svg'],
+		},
+		{
+			first_text:
+				'Инвестируйте в самую быстроразвивающуюся страну Юго-Восточной Азии',
+			second_text:
+				'№1 по росту ВВП, долларизированная экономика, выгодные условия налогообложения, гарантированный возврат инвестиций',
+
+			background: '/mobile/lecondo/lecondo_back.webp',
+			icons: ['/images/icon4.svg', '/images/icon5.svg', '/images/icon6.svg'],
+		},
+	]
+
+	const [currentSlide, setCurrentSlide] = useState(0)
+
+	const nextSlide = () => {
+		const newSlide = (currentSlide + 1) % slides.length
+		setCurrentSlide(newSlide)
+		onChangeSlide(slides[newSlide])
+	}
+
+	const prevSlide = () => {
+		const newSlide = (currentSlide - 1 + slides.length) % slides.length
+		setCurrentSlide(newSlide)
+		onChangeSlide(slides[newSlide])
+	}
+	function getFirstWordAndRest(text) {
+		const trimmedText = text.trim()
+		const words = trimmedText.split(' ')
+		const firstWord = words[0]
+		const restOfText = words.slice(1).join(' ')
+		return { firstWord, restOfText }
+	}
+
+	const progressBarLeft = `${(currentSlide / slides.length) * 10}%`
+
+	return (
+		<div
+			className='investment'
+			style={{ backgroundImage: `url(${slides[currentSlide].background})` }}
+		>
+			<div className='container'>
+				<div className='object_section'>
+					{currentSlide === 0 && (
+						<div>
+							<h2>Wealth Mansion</h2>
+							<ul>
+								<li>
+									<img src='images/icon/Vector.svg' alt='' />
+									<p>
+										45-этажный жилой комплекс бизнес-класса в сердце Пномпеня
+									</p>
+								</li>
+								<li>
+									<img src='images/icon/Vector.svg' alt='' />
+									<p>
+										Предложение с самым высоким прогнозируемым ростом стоимости
+										в Камбодже
+									</p>
+								</li>
+								<li>
+									<img src='images/icon/Vector.svg' alt='' />
+									<p>
+										Роскошный образ жизни и выгодная инвестиция в самом сердце
+										Пномпеня
+									</p>
+								</li>
+								<li>
+									<img src='images/icon/Vector.svg' alt='' />
+									<p>
+										Совершенно новые стандарты жизни в Пномпене, столице
+										Камбоджи
+									</p>
+								</li>
+							</ul>
+						</div>
+					)}
+
+					{currentSlide === 1 && (
+						<div>
+							<h2>le CONDO BKK1</h2>
+
+							<ul>
+								<li>
+									<img src='images/icon/Vector.svg' alt='' />
+									<p>
+										Идеальный вариант для инвестиций с целью получения высокого
+										арендного дохода
+									</p>
+								</li>
+								<li>
+									<img src='images/icon/Vector.svg' alt='' />
+									<p>
+										Высокий спрос из-за идеальной туристической локации: 90%
+										квартир уже приобретено
+									</p>
+								</li>
+								<li>
+									<img src='images/icon/Vector.svg' alt='' />
+									<p>
+										Разумные инвестиции и роскошная жизнь в одном из самых
+										престижных жилых комплексов Пномпеня
+									</p>
+								</li>
+							</ul>
+						</div>
+					)}
+				</div>
+				<div className='slider'>
+					<div className='slider_section'>
+						<button className='slider_button' onClick={prevSlide}>
+							<img src='/images/icon/prev.svg' alt='Previous' />
+						</button>
+						<div className='slider_slide'>
+							<div className='slider_scroll-amount'></div>
+							<div className='slider_scroll-progres'>
+								<div
+									className='slider_scroll-progres_bar'
+									style={{ left: progressBarLeft }}
+								></div>
+							</div>
+						</div>
+						<button className='slider_button' onClick={nextSlide}>
+							<img src='/images/icon/next.svg' alt='Next' />
+						</button>
+					</div>
+					<p>ПРЕИМУЩЕСТВА И ФОТО НИЖЕ</p>
+				</div>
+			</div>
+		</div>
+	)
+}
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,12 +1,16 @@
 import React, { useState } from 'react'
 import { Cambodia } from '../components/Cambodia/Cambodia'
+import { Desc } from '../components/Desc/Desc'
 import Header from '../components/Header'
 import { Investments } from '../components/Investment/Investments'
+import { Object } from '../components/Object/Object'
 import './Home.css'
 
 function Home() {
 	const [background, setBackground] = useState('/images/bg1.webp')
-	const [backgroundCambodia, setBackgroundCambodia] = useState('/mobile/2 cambodia/bg-scroll.webp')
+	const [backgroundCambodia, setBackgroundCambodia] = useState(
+		'/mobile/2 cambodia/bg-scroll.webp'
+	)
 
 	const handleSlideChange = slide => {
 		setBackground(slide.background)
@@ -15,11 +19,21 @@ function Home() {
 		setBackground(slide.background)
 	}
 
+	const handleSlideChangeObject = slide => {
+		setBackground(slide.background)
+	}
+
+	const handleSlideChangeDesc = slide => {
+		setBackground(slide.background)
+	}
+
 	return (
 		<div className='home'>
 			<Header />
-			<Investments  onChangeSlide={handleSlideChange} />
+			<Investments onChangeSlide={handleSlideChange} />
 			<Cambodia id='cambodia' onChangeSlide={handleSlideChangeCambodia} />
+			<Object onChangeSlide={handleSlideChangeObject} />
+			<Desc onChangeSlide={handleSlideChangeDesc} />
 			<div className='middle_button'>
 				<a href=''>узнать больше</a>
 			</div>
