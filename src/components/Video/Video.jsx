6af61d6f2d5330974c9ada82147bc55927bb8cb3
mennuaa <menua777menua@gmail.com>
2024-05-17 19:15:35 +0400

    menua

--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -1,29 +1,45 @@
-import React from 'react'
-import './video.css'
+import React, { useState } from 'react';
+import './video.css';
 
 const Video = () => {
-	function playVideo() {
-		let myVideo = document.getElementById('#video')
-		console.log(myVideo)
-	}
-	return (
-		<div className='video'>
-			<div className='container'>
-				<div className='video_section' id='video'>
-					<p>Посмотрите это впечатляющее</p>
-					<h2>видео</h2>
-					<div className='video_video'>
-						<video width='100%' id='video'>
-							<source src='video/InShot_20240516_224507967.mp4'></source>
-						</video>
-						<button onClick={playVideo()} className='video_button'>
-							<img src='images/icon/play.svg' alt='' />
-						</button>
-					</div>
-				</div>
-			</div>
-		</div>
-	)
-}
+    const [isPlaying, setIsPlaying] = useState(false);
 
-export default Video
+    function playVideo() {
+        let myVideo = document.getElementById('myVideo');
+        if (myVideo) {
+            myVideo.play();
+            setIsPlaying(true);
+        }
+    }
+
+    function pauseVideo() {
+        let myVideo = document.getElementById('myVideo');
+        if (myVideo) {
+            myVideo.pause();
+            setIsPlaying(false);
+        }
+    }
+
+    return (
+        <div className='video'>
+            <div className='container'>
+                <div className='video_section'>
+                    <p>Посмотрите это впечатляющее</p>
+                    <h2>видео</h2>
+                    <div className='video_video'>
+                        <video controls width='100%' id='myVideo' onEnded={pauseVideo}>
+                            <source src='video/InShot_20240516_224507967.mp4'></source>
+                        </video>
+                        {!isPlaying && (
+                            <button onClick={playVideo} className='video_button'>
+                                <img src='images/icon/play.svg' alt='Play' />
+                            </button>
+                        )}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Video;
